effda39d5778414f5c6981ff5e3565713b3e10c0
deepvashisth <deepvashisth7777@gmail.com>
2020-10-22 11:13:02 +0530

    speaker updated

--- client/src/components/Speakers.js
+++ client/src/components/Speakers.js
@@ -1,27 +1,53 @@
 import React from "react";
 import comingSoon from "../images/coming-soon.png";
 import "../css/Speakers.css";
+import Kunal_Kushwaha from "../images/speakers/Kunal_Kushwaha.jpg";
+import Punit_Chawla from "../images/speakers/Punit_Chawla.jpg";
+import Akash_James from "../images/speakers/Akash_James.jpeg";
+import Ahmed_aqib from "../images/speakers/Ahmed_aqib.jpg";
+
 
 function Speakers() {
   return (
     <div className="speakers">
-      <h1>Speakers</h1>
-      <div>
-        {/* <Card source={developers} name="Developers" />
-        <Card source={design} name="Design" />
-        <Card source={curation} name="Curation" />
-        <Card source={query} name="Query Management" />
-        <Card source={organizer} name="Organizers" />
-        <Card source={sponsor} name="sponsors" /> */}
-        <img src={comingSoon} alt="coming-soon" />
+
+      <div className="">
+          <br /><br /><br /><br /><br /><br />
+          <h1 className="ta_center title">Speakers</h1>
+          <br /><br />
+            <div className="">
+              <div className="column large4 medium6 small6">
+                  <img src={Ahmed_aqib}   alt="altimage" />
+                  <br />
+                  <p>Ahmed Aqib</p>
+              </div>
+              <div className="column large4 medium6 small6">
+                <img src={Akash_James}   alt="altimage" />
+                  <br />
+                  <p>Akash James</p>
+                </div>
+            </div>
+            <br /><br />
+          <div className="">
+              <div className="column large4 medium6 small6">
+                  <img src={Kunal_Kushwaha}   alt="altimage" />
+                  <br />
+                  <p>Kunal Kushwaha</p>
+              </div>
+              <div className="column large4 medium6 small6">
+                  <img src={Punit_Chawla}   alt="altimage" />
+                  <br />
+                    <p>Punit Chawla</p>
+              </div>
+          </div>
+
       </div>
-      {/* <button className="primary_blue small">CALL FOR SPEAKERS</button> */}
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
-  );
+
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+  </div>);
 }
 
 export default Speakers;
