3e67d80fa916e3c253a4e989ca9b79f239184d0b
D3LTAxx <adnanyousuff89@gmail.com>
2021-10-14 22:01:49 +0530

    Redirected Speakers to coming soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component} from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "fdweb/fluent.css";
 import "fdweb/fluent-icons.css";
 import "./App.css";
@@ -10,23 +10,22 @@ import Speakers from "./components/Speakers";
 import FAQ from "./components/FAQ";
 import Home from "./components/HomeComp";
 import FourZeroFour from "./components/404Comp";
-import ComingSoon from "./components/ComingSoon"
-
+import ComingSoon from "./components/ComingSoon";
 
 class App extends Component {
   render() {
     return (
       <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/speakers" component={Speakers} />
-            <Route path="/teams" component={Teams} />
-            <Route path="/coc" component={Home} />
-            <Route path="/faqs" component={FAQ} />
-            <Route path="/comingsoon" component={ComingSoon} />
-            <Route component={FourZeroFour} />
-          </Switch>
-          <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/speakers" component={Speakers} /> */}
+          {/* <Route path="/teams" component={Teams} /> */}
+          <Route path="/coc" component={Home} />
+          <Route path="/faqs" component={FAQ} />
+          <Route path="/comingsoon" component={ComingSoon} />
+          <Route component={FourZeroFour} />
+        </Switch>
+        <Navbar />
       </Router>
     );
   }
