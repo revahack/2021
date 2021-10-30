5715d15ab6bf0ee66eff60c0aa4b9848d50641e0
MOHIT-KUMAR <udaykumarveer15@gmail.com>
2021-10-30 14:01:37 +0530

    prizes updated

--- client/src/components/Prizes.js
+++ client/src/components/Prizes.js
@@ -13,16 +13,32 @@ class Prizes extends Component {
           <br />
           <br />
           <br />
+          <h1 className="ta_center title make-it-small">Total Prize Pool of</h1>
+          <h1 className="ta_center title highlight make-it-small">
+            INR 32,00,000
+          </h1>
+
           <br />
           <br />
-          <h1 className="ta_center title">Prizes and Incentives</h1>
+          <br />
+          <br />
+          <h1 className="ta_center title make-it-small">
+            Prizes and Incentives
+          </h1>
           <br />
           <br />
           <br />
           <br />
-          <div className="column large8 medium8 small12">
+          <div className="column large6 medium6 small12">
             <h3 className="ta_center">1st Prize</h3>
             <h1 className="ta_center">INR 20,000</h1>
+            <h3 className="ta_center">+</h3>
+            <h3 className="ta_center make-it-extra-small med-text">
+              Goodies,Swags and a Whole Lot More,
+            </h3>
+            <h3 className="ta_center make-it-extra-small med-text">
+              From Our Lovely Sponsers.
+            </h3>
             <br />
             <br />
             <hr className="large8 medium8 small10" />
